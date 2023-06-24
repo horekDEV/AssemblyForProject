9beffec80005e123a119147b9683785c7e419d61
horek <horektopksta@gmail.com>
2023-06-24 15:02:27 +0300

    fix warp command registration bug :)

--- src/main/java/ru/horekdev/assemblyforproject/AssemblyForProject.java
+++ src/main/java/ru/horekdev/assemblyforproject/AssemblyForProject.java
@@ -3,6 +3,7 @@ package ru.horekdev.assemblyforproject;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.horekdev.assemblyforproject.command.admins_commands.*;
 import ru.horekdev.assemblyforproject.command.admins_commands.warp.Warp;
+import ru.horekdev.assemblyforproject.command.admins_commands.warp.WarpCMD;
 import ru.horekdev.assemblyforproject.command.world_commands.GameModeCMD;
 import ru.horekdev.assemblyforproject.command.world_commands.SetSpawnCMD;
 import ru.horekdev.assemblyforproject.mix.events.OnEvents;
@@ -33,6 +34,7 @@ public final class AssemblyForProject extends JavaPlugin {
         getCommand("gm").setExecutor(new GameModeCMD());
         getCommand("mix").setExecutor(new Mix());
         getCommand("t").setExecutor(new TimeCMD());
+        getCommand("warp").setExecutor(new WarpCMD());
 
         getServer().getPluginManager().registerEvents(new OnEvents(), this);
         getServer().getPluginManager().registerEvents(new BetweenEvents(), this);
